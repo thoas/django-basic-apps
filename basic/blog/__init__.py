72ad0a50a464d3dd296a566f749e86b1f9080bac
kevin fricovsky <kevin@montylounge.com>
2010-02-04 22:52:20 -0500

    adding the signal would be nice

--- basic/blog/__init__.py
+++ basic/blog/__init__.py
@@ -1,5 +1,6 @@
-from basic.blog.models import Settings
-from basic.blog.signals import invalidate_settings_cache
+from basic.blog.models import Settings, Post
+from basic.blog.signals import invalidate_settings_cache, post_to_twitter
 from django.db.models import signals
 
 signals.post_save.connect(invalidate_settings_cache, Settings, True)
+signals.pre_save.connect(post_to_twitter, Post)
